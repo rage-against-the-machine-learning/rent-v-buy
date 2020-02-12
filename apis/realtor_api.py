599408bc4e969d1722d0cbfe9d7b58b8309510ef
Felipe <felipelopez@utexas.edu>
2020-02-11 20:41:27 -0600

    testing APIs

---
+++ apis/realtor_api.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://realtor.p.rapidapi.com/properties/list-for-sale"
+
+querystring = {"sort":"relevance","radius":"10","city":"Austin","offset":"0","limit":"200","state_code":"TX"}
+
+headers = {
+    'x-rapidapi-host': "realtor.p.rapidapi.com",
+    'x-rapidapi-key': "70005b5ee2msh5ea18e4551424e5p168e01jsn6bc7cf8d8fba"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
