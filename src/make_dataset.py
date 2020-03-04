80cd9ac23b1af5d7566c9048069d2c571281083a
godsylla <sylvi.tran@gmail.com>
2020-03-04 13:07:59 -0800

    extracted data

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,3 +1,26 @@
 import pandas as pd 
 import numpy as np  
+from zipfile import ZipFile
+
+import pathlib
+import sys
+import os 
+sys.path.append(str(pathlib.Path().absolute().parent))
+
+
+# Unzip the files and read the csvs
+csv_dfs = {}
+
+for filename in sorted(os.listdir('../data/raw/')):
+    if '.csv.zip' in filename: 
+        with ZipFile(f'../data/raw/{filename}', 'r') as a_zip:
+            a_zip.extractall(f'../data/raw/unzipped/{filename[:-4]}')
+            
+    elif '.csv' in filename:
+        csv_dfs[filename] = pd.read_csv(f'../data/raw/{filename}')
+        
+# Read in the extracted zip files       
+for filename in sorted(os.listdir('../data/raw/unzipped/')):
+    csv_dfs[filename] = (pd.read_csv(f'../data/raw/unzipped/{filename}/{filename}', engine='python'))
+
 
