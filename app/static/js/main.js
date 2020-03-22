938d40d74252193ff9b4f7ddc0facbb1344e96f9
knail1 <oansari@gmail.com>
2020-03-22 15:57:25 -0500

    showing only CA but somethings off

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -7,29 +7,42 @@ var	margin = {top: 30, right: 40, bottom: 30, left: 70},
     //height = 330 - margin.top - margin.bottom;
     height = 330;
 
-const projection = d3.geoAlbersUsa()
+const projectionUSA = d3.geoAlbersUsa()
      .translate([width/2, height/2])    /* translate to center of screen */
      .scale(700); /* scale things down so see entire US */
 
-    var geoPauth = d3.geoPath().projection(projection);
+			var projectionCA = d3.geoMercator()
+								   .center([ -120, 37 ])
+								   .translate([ width/2, height/2 +40 ])
+								   .scale([ width*2.5 ]);
+
+    var geoPauth = d3.geoPath().projection(projectionCA);
 
     var choroplethSVG = d3.select("#choroplethSVG")
         //.attr("width", width)
         //.attr("height", height);
     var g = choroplethSVG.append("g")
-
-    d3.json("static/maps/zips_us_topo.json").then(function(data){
+    var allCAData;
+    //var objectName = "California.geo"
+    //d3.json("static/maps/zips_us_topo.json").then(function(data){
+        //d3.json("static/maps/California.topo.json").then(function(data){
+            d3.json("static/maps/California.topo_backup.json").then(function(data){
+                // the backup file changes the object name from California.geo to Californiageo so the data() doesn't error out
+            allCAData = data
+            console.log(data)
             //Promise.all(promises).then(function(data) {
             g.selectAll("path")
-                .data(topojson.feature(data, data.objects.zip_codes_for_the_usa).features)
+                //.data(topojson.feature(data, data.objects.zip_codes_for_the_usa).features)
+                .data(topojson.feature(data, data.objects.Californiageo).features)
                 .enter()
                 .append("path")
                 .attr("stroke", "#333")
                 .attr("class", "counties")
                 .attr("class", "zip")
-                .attr("data-zip", function(d) {return d.properties.zip; })
-                .attr("data-state", function(d) {return d.properties.state; })
-                .attr("data-name", function(d) {return d.properties.name; })
+                .attr("data-zip", function(d) {return d.properties.id; })
+                //.attr("data-zip", function(d) {return d.properties.zip; })
+                //.attr("data-state", function(d) {return d.properties.state; })
+                //.attr("data-name", function(d) {return d.properties.name; })
                 .attr("d", geoPauth);
         }) ;
 
