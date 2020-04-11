94ec5b57539537fb41ede44341cc432544ec95ce
Felipe <felipelopez@utexas.edu>
2020-04-11 17:21:17 -0500

    updated ranges of financial sliders

--- app/static/js/financialSliders.js
+++ app/static/js/financialSliders.js
@@ -4,14 +4,14 @@ Show only if a zip code has already been selected
 */
 function showSliders(){
     // Down payment slider
-    var down_payment_data = [0.0, 50000.0, 100000.0, 150000.0, 200000.0];
+    var down_payment_data = [0.0, 100000.0, 200000.0, 300000.0, 400000.0];
     var downPaymentSlider = d3
         .sliderBottom()
         .min(d3.min(down_payment_data)).max(d3.max(down_payment_data))
         .width(300)
         .tickFormat(d3.format('($,d'))
         .ticks(5)
-        .default(40000.0)
+        .default(100000.0)
         .on('onchange', val => { d3.select('p#value-down-payment').text(d3.format('($d')(val)); });
     var gSimple = d3
         .select('div#slider-down-payment')
@@ -22,14 +22,14 @@ function showSliders(){
     gSimple.call(downPaymentSlider);
     d3.select('p#value-down-payment').text(d3.format('($d')(downPaymentSlider.value()));
     // Maintenance and HOA fees slider
-    var annual_maintenance_data = [0.0, 5000.0, 10000.0, 15000.0, 20000.0];
+    var annual_maintenance_data = [0.0, 10000.0, 20000.0, 30000.0, 40000.0];
     var maintenanceSlider = d3
         .sliderBottom()
         .min(d3.min(annual_maintenance_data)).max(d3.max(annual_maintenance_data))
         .width(300)
         .tickFormat(d3.format('($,d'))
         .ticks(5)
-        .default(10000.0)
+        .default(20000.0)
         .on('onchange', val => { d3.select('p#value-annual-maintenance').text(d3.format('($d')(val)); });
     var gSimple = d3
         .select('div#slider-annual-maintenance')
@@ -40,14 +40,14 @@ function showSliders(){
     gSimple.call(maintenanceSlider);
     d3.select('p#value-annual-maintenance').text(d3.format('($d')(maintenanceSlider.value()));
     // Home insurance fees slider
-    var annual_home_insurance_data = [0.0, 1000.0, 2000.0, 3000.0, 4000.0, 5000.0];
+    var annual_home_insurance_data = [0.0, 2000.0, 4000.0, 6000.0, 8000.0, 10000.0];
     var homeInsuranceSlider = d3
         .sliderBottom()
         .min(d3.min(annual_home_insurance_data)).max(d3.max(annual_home_insurance_data))
         .width(300)
         .tickFormat(d3.format('($,d'))
         .ticks(5)
-        .default(3000.0)
+        .default(6000.0)
         .on('onchange', val => { d3.select('p#value-annual-home-insurance').text(d3.format('($d')(val)); });
     var gSimple = d3
         .select('div#slider-annual-home-insurance')
